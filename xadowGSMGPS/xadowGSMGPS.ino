d8e27f44e20b21f9b8bc85a14fe56e1686d2bb5f
DrCord <cord42@gmail.com>
2016-05-19 09:32:14 -0700

    update program from proof of concept to actual working MVP

--- xadowGSMGPS/xadowGSMGPS.ino
+++ xadowGSMGPS/xadowGSMGPS.ino
@@ -1,78 +1,85 @@
 /*
-    Traceable Dog Collar Application 
-    By AngryCat @ 30-10-2015
+    Eugene Maker Space NearSpace Balloon Project Module
+    GPS Locator Program Module
+    By DrCord @ 5-19-2016
     
-   This program is for the Traceable Dog Collar Application that is 
-   built with Xadow GSM+BLE, Xadow Audio and Xadow GPS v2. 
-   Functions achieved:
-   1. Your RePhone (on the dog collar) will automatically  pick phone calls from any cell phone number
-   2. A message with the content 'GPS' will trigger your RePhone to send the GPS information back to the sender
+   This program is for the GPS Locator Program Module that is built 
+   with the Xadow GSM+BLE and Xadow GPS v2 modules from the RePhone GEO kit by Seeed.
+   http://www.seeedstudio.com/wiki/Rephone
+   
+   Program Functionality:
+   1. The device regularly sends it's GPS data via serial USB in a parseable format.
+   2. A message with the content 'GPS' (case insensitive) will trigger your RePhone
+      to send the GPS information back to the sender.
 */
 
-//Include the necessary libraries 
+// Include the necessary libraries.
 #include <LCheckSIM.h>
 #include <LGSM.h>
 #include <LGPS.h>
 #include <stdio.h>
 #include <string.h>
 
-
 char num[20] = {0};
 char buf_contex[100];
+char buffer[200] = {0,};
+unsigned char *utc_date_time = 0;
+int loopCounter = 0;
 
-//Set up
-void setup()
-{
+void setup(){
     Serial.begin(115200);
     Serial.print("\r\n");
-    Serial.print("GPS Dog Collar Test\r\n");   
+    Serial.print("GPS Locator Module Online\r\n");
 }
 
 //set a loop
-void loop()
-{
-    char buffer[200] = {0,};
+void loop(){
     unsigned KeyValue = 0;
-    unsigned char *utc_date_time = 0;
 
-    if(LVoiceCall.getVoiceCallStatus() == RECEIVINGCALL)              // if receives an incoming call
-    {  
-        LVoiceCall.retrieveCallingNumber(num,20);
-        sprintf(buffer, "Call come in, number is %s \r\n", num);
-        Serial.println(buffer);
-        LVoiceCall.answerCall();                                      // pick up the phone call
-    }
-    
-    if(LSMS.available())                                               // if received a SMS from another cellphone
-    {
+    // If received a SMS from another cellphone.
+    if(LSMS.available()){
         LSMS.remoteNumber(num, 20); 
         LSMS.remoteContent(buf_contex, 50);
         
-        sprintf(buffer, "Get new sms, content: %s, number: %s \r\n", buf_contex, num);
+        sprintf(buffer, "Received new sms, content: %s, number: %s \r\n", buf_contex, num);
         Serial.println(buffer);
-        
-        if(strcmp("GPS",(char*)buf_contex) == 0)                       // if the SMS content is 'GPS', then send the GPS information of RePhone back to the sender
-        {
-            if(LGPS.check_online())
-            {
-                utc_date_time = LGPS.get_utc_date_time();
-                sprintf(buffer, "GPS list:\r\nUTC:%d-%d-%d  %d:%d:%d,\r\nlatitude: %c:%f,\r\nlongitude: %c:%f,\r\naltitude: %f,\r\nspeed: %f,\r\ncourse: %f.", 
-                                 utc_date_time[0], utc_date_time[1], utc_date_time[2], utc_date_time[3], utc_date_time[4], utc_date_time[5], 
-                                 LGPS.get_ns(), LGPS.get_latitude(), LGPS.get_ew(), LGPS.get_longitude(), LGPS.get_altitude(), LGPS.get_speed(), LGPS.get_course());
-                Serial.println(buffer);
+
+        // If the SMS content is 'GPS' (case insensitive), 
+        // then send the GPS information of device back to the sender.
+        if(String("GPS").equalsIgnoreCase((char*)buf_contex)){
+            if(LGPS.check_online()){
+              outputGPS();
+              Serial.println(buffer);
                 
-                if(LSMS.ready())
-                {
+              if(LSMS.ready()){
                     LSMS.beginSMS(num);
                     LSMS.print(buffer);
                     
                     if(LSMS.endSMS()) Serial.println("SMS sent ok!");
                     else Serial.println("SMS send fail!");
-                }
-                else Serial.println("SMS no ready!");
+              }
+              else Serial.println("SMS not ready!");
             }
         }
         LSMS.flush();
     }
-    delay(100);   
+    // Write GPS location to serial every 20 loops
+    if(loopCounter > 20){
+      outputGPS();
+      Serial.println(buffer);
+      loopCounter = 0;
+    }
+    else{
+      loopCounter++;
+    }
+    delay(100);  
+}
+
+void outputGPS(){
+  if(LGPS.check_online()){
+    utc_date_time = LGPS.get_utc_date_time();
+    sprintf(buffer, "GPS list:\r\nUTC:%d-%d-%d  %d:%d:%d,\r\nlatitude: %c:%f,\r\nlongitude: %c:%f,\r\naltitude: %f,\r\nspeed: %f,\r\ncourse: %f.", 
+                       utc_date_time[0], utc_date_time[1], utc_date_time[2], utc_date_time[3], utc_date_time[4], utc_date_time[5], 
+                       LGPS.get_ns(), LGPS.get_latitude(), LGPS.get_ew(), LGPS.get_longitude(), LGPS.get_altitude(), LGPS.get_speed(), LGPS.get_course());
+  }
 }
